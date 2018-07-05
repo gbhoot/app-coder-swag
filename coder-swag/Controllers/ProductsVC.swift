5c65b56b9fd82f92ec7d846677601265771077d8
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-07-05 15:55:11 -0700

    completed app, added segue and popilated product pages

---
+++ coder-swag/Controllers/ProductsVC.swift
@@ -0,0 +1,47 @@
+//
+//  ProductsVC.swift
+//  coder-swag
+//
+//  Created by Gurpreet Bhoot on 7/5/18.
+//  Copyright © 2018 Gurpal Bhoot. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var productsCollection: UICollectionView!
+    private(set) public var products = [Product]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func initProducts(category: Category) {
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        
+        return ProductCell()
+    }
+}
