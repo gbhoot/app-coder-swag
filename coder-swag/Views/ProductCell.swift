11aeef6102205a7a12029585c4af986554cad775
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-07-05 15:07:07 -0700

    added collection view cell for product pages

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Gurpreet Bhoot on 7/5/18.
+//  Copyright © 2018 Gurpal Bhoot. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
